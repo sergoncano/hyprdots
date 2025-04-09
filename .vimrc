40a4773592d4129b05352ed993bf09cad79393f1
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-04-10 00:05:59 +0200

    First version

---
+++ .vimrc
@@ -0,0 +1,5 @@
+highlight Normal guibg=none
+highlight NonText guibg=none
+highlight Normal ctermbg=none
+highlight NonText ctermbg=none
+
---
+++ nvim/plugins.lua
@@ -0,0 +1,29 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'xiyaowong/transparent.nvim'
+
+  -- Lazy loading:
+  -- Load on specific commands
+  -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+
+  -- Load on an autocommand event
+  -- use {'andymass/vim-matchup', event = 'VimEnter'}
+
+  -- Load on a combination of conditions: specific filetypes or commands
+  -- Also run code after load (see the "config" key)
+
+  -- Plugins can have dependencies on other plugins
+
+  -- Plugins can also depend on rocks from luarocks.org:
+
+  -- You can specify rocks in isolation
+
+
+  -- You can alias plugin names
+end)
