9fe11f490be34a3fa92e881450a633044bc1d4d2
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-04-12 00:10:55 +0200

    Added powermenu support, waybar tweaks among others

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -7,6 +7,18 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'xiyaowong/transparent.nvim'
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+                            , branch = '0.1.x',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+	}
+  use {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup {}
+    end
+  }
 
   -- Lazy loading:
   -- Load on specific commands
