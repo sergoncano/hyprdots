a6e82aa0b3a0b3db436f08b40081b319a7709a6b
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-04-25 21:29:50 +0200

    Added assets and fixed yet another batch of bugs in the installer

--- installer.sh
+++ installer.sh
@@ -1,8 +1,13 @@
 echo "This installer will prompt you to choose which features of the hyprdots you want installed on your system. 
-Please read the code carefully before executing with  permissions."
+Please read the code carefully before executing with elevated privileges."
 echo 
 
+
+if [[ $(whoami) =~ "root" ]]; then
+
 cd $(dirname "$0")
+
+HOMEDIR=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 pacman -Sy
 
@@ -23,23 +28,28 @@ if [[ $hyprland =~ [Yy]$ ]]; then
 
 	read -p "Do you want the dunst color scheme to change?[Y/n]" dunst
 	if [[ $dunst =~ [Yy]$ ]]; then
-		mv ~/.config/dunst/dunstrc ~/.config/dunst/dunstrc.backup
-		ln -s ~/hyprdots/dunstrc ~/.config/dunst/dunstrc
+		mv $HOMEDIR/.config/dunst/dunstrc $HOMEDIR/.config/dunst/dunstrc.backup
+		ln -s $HOMEDIR/hyprdots/dunstrc $HOMEDIR/.config/dunst/dunstrc
 	fi
 	# For certain menus
 	pacman -S --needed rofi-wayland
-	mkdir ~/.config/rofi
-	mv ~/.config/rofi/config.rasi ~/.config/rofi/config.rasi.backup
-	ln -s ~/hyprdots/rofi/config.rasi ~/.config/rofi/
-	ln -s ~/hyprdots/rofi/launchpad.rasi ~/.config/rofi/
-	ln -s ~/hyprdots/rofi/hiddenFiles.rasi ~/.config/rofi/
-	ln -s ~/hyprdots/rofi/wallpaperPicker.rasi ~/.config/rofi/
-	mv ~/.config/wal/templates/colors-rofi-light.rasi ~/.config/wal/templates/colors-rofi-light.rasi.backup
-	ln -s ~/hyprdots/rofi/templates/colors-rofi-light.rasi ~/.config/templates/
+	mkdir $HOMEDIR/.config/rofi
+	mv $HOMEDIR/.config/rofi/config.rasi $HOMEDIR/.config/rofi/config.rasi.backup
+	ln -s $HOMEDIR/hyprdots/rofi/config.rasi $HOMEDIR/.config/rofi/
+	ln -s $HOMEDIR/hyprdots/rofi/launchpad.rasi $HOMEDIR/.config/rofi/
+	ln -s $HOMEDIR/hyprdots/rofi/hiddenFiles.rasi $HOMEDIR/.config/rofi/
+	ln -s $HOMEDIR/hyprdots/rofi/wallpaperPicker.rasi $HOMEDIR/.config/rofi/
+	mv $HOMEDIR/.config/wal/templates/colors-rofi-light.rasi $HOMEDIR/.config/wal/templates/colors-rofi-light.rasi.backup
+	ln -s $HOMEDIR/hyprdots/rofi/templates/colors-rofi-light.rasi $HOMEDIR/.config/templates/
 
 	# For screenshotting
-	pacman -S --needed hyprpicker hyprshot 
-
+    
+	read -p "Do you want software for screenshotting? yay will be installed if it is not already present[Y/n]" screenshots
+	if [[ $screenshots =~ [Yy]$ ]]; then
+        pacman -S --needed hyprpicker 
+		pacman -S --needed yay
+        yay -Sc hyprshot
+    fi
 	# For playing media
 	pacman -S --needed playerctl
 	
@@ -50,25 +60,31 @@ if [[ $hyprland =~ [Yy]$ ]]; then
 	if [[ $eww =~ [Yy]$ ]]; then
 		# Widgets 
 		pacman -S --needed yay
-		yay -S eww
-		mkdir ~/.config/eww
-		mv ~/.config/eww/eww.yuck ~/.config/eww/eww.yuck.backup
-		mv ~/.config/eww/eww.scss ~/.config/eww/eww.scss.backup
-		ln -s ~/hyprdots/eww/eww.yuck ~/.config/eww/
-		ln -s ~/hyprdots/eww/eww.scss ~/.config/eww/
+		yay -Sc eww
+		mkdir $HOMEDIR/.config/eww
+		mv $HOMEDIR/.config/eww/eww.yuck $HOMEDIR/.config/eww/eww.yuck.backup
+		mv $HOMEDIR/.config/eww/eww.scss $HOMEDIR/.config/eww/eww.scss.backup
+		ln -s $HOMEDIR/hyprdots/eww/eww.yuck $HOMEDIR/.config/eww/
+		ln -s $HOMEDIR/hyprdots/eww/eww.scss $HOMEDIR/.config/eww/
 		for d in eww/*/; do
-			mkdir ~/.config/eww/$d
-			ln -s ~/hyprdots/eww/$d.yuck ~/.config/eww/$d/
-			ln -s ~/hyprdots/eww/$d.scss ~/.config/eww/$d/
+			mkdir $HOMEDIR/.config/eww/$d
+			ln -s $HOMEDIR/hyprdots/eww/$d.yuck $HOMEDIR/.config/eww/$d/
+			ln -s $HOMEDIR/hyprdots/eww/$d.scss $HOMEDIR/.config/eww/$d/
 		done
 	fi
 
-	ln -s ~/hyprdots/scripts/changeWallpaper /usr/local/bin/
-	ln -s ~/hyprdots/scripts/toggleWaybar  /usr/local/bin/
-	mkdir ~/.config/hypr/
-	mv ~/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf.backup
-	ln -s ~/hyprdots/hypr/hyprland.conf ~/.config/hypr/
-	hyprctl reload
+	ln -s $HOMEDIR/hyprdots/scripts/changeWallpaper /usr/local/bin/
+	ln -s $HOMEDIR/hyprdots/scripts/toggleWaybar  /usr/local/bin/
+	mkdir $HOMEDIR/.config/hypr/
+	mv $HOMEDIR/.config/hypr/hyprland.conf $HOMEDIR/.config/hypr/hyprland.conf.backup
+	ln -s $HOMEDIR/hyprdots/hypr/hyprland.conf $HOMEDIR/.config/hypr/
+	    
+    echo "What keyboard layout do you wish to use?"
+    read -p "Mind capital letters, if you mispell it you might have a hard time reconfiguring it." layout
+    sed -i --follow-symlinks "s|kb_layout = es|kb_layout = $layout|" $HOMEDIR/hyprdots/hypr/hyprland.conf
+
+
+    hyprctl reload
 fi
 
 echo
@@ -76,22 +92,25 @@ echo
 read -p "Do you want the hyprlock/hypridle config installed?[Y/n]" hyprlock
 if [[ $hyprlock =~ [Yy]$ ]]; then
 	pacman -S --needed hypridle hyprlock
-	mkdir ~/.config/hypr/
-	mv ~/.config/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf.backup
-	mv ~/.config/hypr/hypridle.conf ~/.config/hypr/hypridle.conf.backup
-	ln -s ~/hyprdots/hypr/hyprlock.conf ~/.config/hypr/
-	ln -s ~/hyprdots/hypr/hypridle.conf ~/.config/hypr/
-	cp "The Wild Breath of Zelda.otf" ~/.local/share/fonts/
-	systemctl --user enable --now hypridle
+	mkdir $HOMEDIR/.config/hypr/
+	mv $HOMEDIR/.config/hypr/hyprlock.conf $HOMEDIR/.config/hypr/hyprlock.conf.backup
+	mv $HOMEDIR/.config/hypr/hypridle.conf $HOMEDIR/.config/hypr/hypridle.conf.backup
+	ln -s $HOMEDIR/hyprdots/hypr/hyprlock.conf $HOMEDIR/.config/hypr/
+	ln -s $HOMEDIR/hyprdots/hypr/hypridle.conf $HOMEDIR/.config/hypr/
+	cp $HOMEDIR/hyprdots/assets/"The Wild Breath of Zelda.otf" $HOMEDIR/.local/share/fonts/
+    mkdir $HOMEDIR/Pictures/
+    mkdir $HOMEDIR/Pictures/walllpapers/
+	ln -s $HOMEDIR/hyprdots/assets/lockscreen.jpg $HOMEDIR/Pictures/wallpapers/
+    systemctl --user enable --now hypridle
 fi
 
 echo
 
 read -p "Do you want the kitty config installed?[Y/n]" kitty
 if [[ $kitty =~ [Yy]$ ]]; then
-	mkdir ~/.config/kitty/
-	mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.backup
-	ln -s ~/hyprdots/kitty/kitty.conf ~/.config/kitty/
+	mkdir $HOMEDIR/.config/kitty/
+	mv $HOMEDIR/.config/kitty/kitty.conf $HOMEDIR/.config/kitty/kitty.conf.backup
+	ln -s $HOMEDIR/hyprdots/kitty/kitty.conf $HOMEDIR/.config/kitty/
 fi
 
 echo
@@ -99,17 +118,20 @@ echo
 read -p "Do you want the waybar config installed?[Y/n]" waybar
 if [[ $waybar =~ [Yy]$ ]]; then
 	pacman -S --needed waybar ttf-font-awesome
-	mkdir ~/.config/waybar
-	mv ~/.config/waybar/config.jsonc ~/.config/waybar/config.jsonc.backup
-	mv ~/.config/waybar/style.css ~/.config/waybar/style.css.backup
-	ln -s ~/hyprdots/waybar/config.jsonc ~/.config/waybar/
-	ln -s ~/hyprdots/waybar/style.css ~/.config/waybar/
+	mkdir $HOMEDIR/.config/waybar
+	mv $HOMEDIR/.config/waybar/config.jsonc $HOMEDIR/.config/waybar/config.jsonc.backup
+	mv $HOMEDIR/.config/waybar/style.css $HOMEDIR/.config/waybar/style.css.backup
+	ln -s $HOMEDIR/hyprdots/waybar/config.jsonc $HOMEDIR/.config/waybar/
+	ln -s $HOMEDIR/hyprdots/waybar/style.css $HOMEDIR/.config/waybar/
+    sed -i --follow-symlinks "s|/home/sergio/hyprdots/waybar/republicLogo.png|$HOMEDIR/hyprdots/waybar/republicLogo.png|"
 fi
 
 read -p "Do you want the bash config installed?[Y/n]" bashconf 
 if [[ $bashconf =~ [Yy]$ ]]; then
-	mv ~/.bashrc ~/.bashrc.backup
-	ln -s ~/hyprdots/.bashrc ~/.bashrc
+	mv $HOMEDIR/.bashrc $HOMEDIR/.bashrc.backup
+	ln -s $HOMEDIR/hyprdots/.bashrc $HOMEDIR/.bashrc
 fi
-
-echo "REMEMBER TO CHANGE YOUR KEYBOARD DISTRO MANUALLY IN HYPRLAND.CONF"
+hyprctl reload
+else
+    echo "This script must be run as root!"
+fi
