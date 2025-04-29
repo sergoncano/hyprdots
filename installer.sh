3ca253a6f325dc137e9fe8c57a32e77ad431b043
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-04-29 11:56:16 +0200

    installer minor fix

--- installer.sh
+++ installer.sh
@@ -43,13 +43,13 @@ if [[ $hyprland =~ [Yy]$ ]]; then
 
     # For certain menus
 	pacman -S --needed rofi-wayland
-	mkdir $HOMEDIR/.config/rofi
+	mkdir -p $HOMEDIR/.config/rofi
 	mv $HOMEDIR/.config/rofi/config.rasi $HOMEDIR/.config/rofi/config.rasi.backup
 	ln -s $HOMEDIR/hyprdots/rofi/config.rasi $HOMEDIR/.config/rofi/
 	ln -s $HOMEDIR/hyprdots/rofi/launchpad.rasi $HOMEDIR/.config/rofi/
 	ln -s $HOMEDIR/hyprdots/rofi/hiddenFiles.rasi $HOMEDIR/.config/rofi/
 	ln -s $HOMEDIR/hyprdots/rofi/wallpaperPicker.rasi $HOMEDIR/.config/rofi/
-    mkdir $HOMEDIR/.config/wal/templates/
+    mkdir -p $HOMEDIR/.config/wal/templates/
 	mv $HOMEDIR/.config/wal/templates/colors-rofi-light.rasi $HOMEDIR/.config/wal/templates/colors-rofi-light.rasi.backup
 	ln -s $HOMEDIR/hyprdots/rofi/templates/colors-rofi-light.rasi $HOMEDIR/.config/wal/templates/
     sed -i --follow-symlinks "s|/home/sergio/Pictures/wallpapers|$HOMEDIR/Pictures/wallpapers|" $HOMEDIR/hyprdots/rofi/wallpaperPicker.rasi
@@ -68,13 +68,13 @@ if [[ $hyprland =~ [Yy]$ ]]; then
 		# Widgets 
 		pacman -S --needed yay
 		yay -Sc eww
-		mkdir $HOMEDIR/.config/eww
+		mkdir -p $HOMEDIR/.config/eww
 		mv $HOMEDIR/.config/eww/eww.yuck $HOMEDIR/.config/eww/eww.yuck.backup
 		mv $HOMEDIR/.config/eww/eww.scss $HOMEDIR/.config/eww/eww.scss.backup
 		ln -s $HOMEDIR/hyprdots/eww/eww.yuck $HOMEDIR/.config/eww/
 		ln -s $HOMEDIR/hyprdots/eww/eww.scss $HOMEDIR/.config/eww/
 		for d in eww/*/; do
-			mkdir $HOMEDIR/.config/eww/$d
+			mkdir -p $HOMEDIR/.config/eww/$d
 			ln -s $HOMEDIR/hyprdots/eww/$d.yuck $HOMEDIR/.config/eww/$d/
 			ln -s $HOMEDIR/hyprdots/eww/$d.scss $HOMEDIR/.config/eww/$d/
 		done
@@ -85,7 +85,7 @@ if [[ $hyprland =~ [Yy]$ ]]; then
 	
     ln -s $HOMEDIR/hyprdots/scripts/changeWallpaper /usr/local/bin/
 	ln -s $HOMEDIR/hyprdots/scripts/toggleWaybar  /usr/local/bin/
-	mkdir $HOMEDIR/.config/hypr/
+	mkdir -p $HOMEDIR/.config/hypr/
 	mv $HOMEDIR/.config/hypr/hyprland.conf $HOMEDIR/.config/hypr/hyprland.conf.backup
 	ln -s $HOMEDIR/hyprdots/hypr/hyprland.conf $HOMEDIR/.config/hypr/
 	    
@@ -102,15 +102,14 @@ echo
 read -p "Do you want the hyprlock/hypridle config installed?[Y/n]" hyprlock
 if [[ $hyprlock =~ [Yy]$ ]]; then
 	pacman -S --needed hypridle hyprlock
-	mkdir $HOMEDIR/.config/hypr/
+	mkdir -p $HOMEDIR/.config/hypr/
 	mv $HOMEDIR/.config/hypr/hyprlock.conf $HOMEDIR/.config/hypr/hyprlock.conf.backup
 	mv $HOMEDIR/.config/hypr/hypridle.conf $HOMEDIR/.config/hypr/hypridle.conf.backup
 	ln -s $HOMEDIR/hyprdots/hypr/hyprlock.conf $HOMEDIR/.config/hypr/
 	ln -s $HOMEDIR/hyprdots/hypr/hypridle.conf $HOMEDIR/.config/hypr/
-	mkdir $HOMEDIR/.local/share/fonts/
+	mkdir -p $HOMEDIR/.local/share/fonts/
 	cp $HOMEDIR/hyprdots/assets/"The Wild Breath of Zelda.otf" $HOMEDIR/.local/share/fonts/
-    mkdir $HOMEDIR/Pictures/
-    mkdir $HOMEDIR/Pictures/walllpapers/
+    mkdir -p $HOMEDIR/Pictures/walllpapers/
 	ln -s $HOMEDIR/hyprdots/assets/lockscreen.jpg $HOMEDIR/Pictures/wallpapers/
     systemctl --user enable --now hypridle
 fi
@@ -119,7 +118,7 @@ echo
 
 read -p "Do you want the kitty config installed?[Y/n]" kitty
 if [[ $kitty =~ [Yy]$ ]]; then
-	mkdir $HOMEDIR/.config/kitty/
+	mkdir -p $HOMEDIR/.config/kitty/
 	mv $HOMEDIR/.config/kitty/kitty.conf $HOMEDIR/.config/kitty/kitty.conf.backup
 	ln -s $HOMEDIR/hyprdots/kitty/kitty.conf $HOMEDIR/.config/kitty/
 fi
@@ -129,7 +128,7 @@ echo
 read -p "Do you want the waybar config installed?[Y/n]" waybar
 if [[ $waybar =~ [Yy]$ ]]; then
 	pacman -S --needed waybar ttf-font-awesome
-	mkdir $HOMEDIR/.config/waybar
+	mkdir -p $HOMEDIR/.config/waybar
 	mv $HOMEDIR/.config/waybar/config.jsonc $HOMEDIR/.config/waybar/config.jsonc.backup
 	mv $HOMEDIR/.config/waybar/style.css $HOMEDIR/.config/waybar/style.css.backup
 	ln -s $HOMEDIR/hyprdots/waybar/config.jsonc $HOMEDIR/.config/waybar/
