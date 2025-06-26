8cf1e1ab005fc647cfb877e3fceeb54b441b0201
sergoncano <36008895+sergoncano@users.noreply.github.com>
2025-06-26 14:19:56 +0200

    QOL changes and installer fix

--- installer.sh
+++ installer.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 echo "This installer will prompt you to choose which features of the hyprdots you want installed on your system. 
-The script requires yay installed on your system. If using another aur sudo pacman wrapper, please edit this file."
+The script requires yay installed on your system. If using another aur pacman wrapper, please edit this file."
 echo
 
 if [ "$EUID" -eq 0 ]; then
     echo "Do not run this script as sudo. Please read what it does"
     exit
+fi
+
+if [[ "$(pacman -Ssq lib32-wayland)" == "" ]]; then
+    echo "Please enable the multilib repository, otherwise the script will not work."
+    exit
+fi
+
+if [[ "$(pacman -Qq yay-bin)" == "" ]]; then
+    echo "Yay was not found, it will be installed now"
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd -
 fi
 
 cd $(dirname "$0")
@@ -15,6 +25,7 @@ HOMEDIR="/home/$(whoami)"
 read -p "Do you want the general hyprland config installed?[Y/n]" hyprland
 if [[ $hyprland =~ [Yy]$ ]]; then
     # For certain menus
+    echo "Installing app launcher..."
     sudo pacman -S --needed rofi-wayland
     mkdir -p $HOMEDIR/.config/rofi
     sudo mv $HOMEDIR/.config/rofi/config.rasi $HOMEDIR/.config/rofi/config.rasi.backup
@@ -27,8 +38,10 @@ if [[ $hyprland =~ [Yy]$ ]]; then
     ln -s $HOMEDIR/hyprdots/rofi/templates/colors-rofi-light.rasi $HOMEDIR/.config/wal/templates/
     sed -i --follow-symlinks "s|/home/sergio/Pictures/wallpapers|$HOMEDIR/Pictures/wallpapers|" $HOMEDIR/hyprdots/rofi/wallpaperPicker.rasi
     # For playing media
+    echo "Installing audio player..."
     sudo pacman -S --needed playerctl
 
+    echo "Installing wallpaper manager and add-ons..."
     sudo pacman -S --needed python-pywal
 
     # Wallpaper
@@ -37,68 +50,72 @@ if [[ $hyprland =~ [Yy]$ ]]; then
     # For unit file management
     sudo pacman -S --needed uwsm
 
-    ln -s $HOMEDIR/hyprdots/scripts/changeWallpaper /usr/local/bin/
-    ln -s $HOMEDIR/hyprdots/scripts/toggleWaybar /usr/local/bin/
+    sudo ln -s $HOMEDIR/hyprdots/scripts/changeWallpaper /usr/local/bin/
+    sudo chmod +x /usr/local/bin/changeWallpaper
+    sudo ln -s $HOMEDIR/hyprdots/scripts/toggleWaybar /usr/local/bin/
+    sudo chmod +x /usr/local/bin/toggleWaybar
     mkdir -p $HOMEDIR/.config/hypr/
     mv $HOMEDIR/.config/hypr/hyprland.conf $HOMEDIR/.config/hypr/hyprland.conf.backup
     ln -s $HOMEDIR/hyprdots/hypr/hyprland.conf $HOMEDIR/.config/hypr/
 
     read -p "What keyboard layout do you wish to use? " layout
     sed -i --follow-symlinks "s|kb_layout = es|kb_layout = $layout|" $HOMEDIR/hyprdots/hypr/hyprland.conf
-
-    # For brightness
-    read -p "Do you want software for brightness control?[Y/n]" light
-    if [[ $light =~ [Yy]$ ]]; then
-        yay -S light
-    fi
-
-    # For audio control
-    read -p "Do you want software for audio control?[Y/n]" audio
-    if [[ $audio =~ [Yy]$ ]]; then
-        sudo pacman -S --needed pamixer pipewire-alsa wireplumber pipewire-pulse
-        systemctl --user --now enable pipewire pipewire-pulse wireplumber
-    fi
-
-    # For screenshotting
-    read -p "Do you want software for screenshotting?[Y/n]" screenshots
-    if [[ $screenshots =~ [Yy]$ ]]; then
-        sudo pacman -S --needed hyprpicker
-        yay -S hyprshot
-    fi
-
-    # For notification colors
-    read -p "Do you want the dunst color scheme to change?[Y/n]" dunst
-    if [[ $dunst =~ [Yy]$ ]]; then
-        mkdir -p $HOMEDIR/.config/dunst/
-        mv $HOMEDIR/.config/dunst/dunstrc $HOMEDIR/.config/dunst/dunstrc.backup
-        ln -s $HOMEDIR/hyprdots/dunstrc $HOMEDIR/.config/dunst/
-        echo "Dunst has NOT been installed, do it manually if you haven't yet."
-    fi
-
-    #For the powermenu and certain other widgets
-    read -p "Do you want widgets installed?[Y/n]" eww
-    if [[ $eww =~ [Yy]$ ]]; then
-        # Widgets
-        read -p "Eww will take a longer time to get installed since it is a rust project. Press any key to proceed." dummie
-        yay -S eww
-        mkdir -p $HOMEDIR/.config/eww
-        mv $HOMEDIR/.config/eww/eww.yuck $HOMEDIR/.config/eww/eww.yuck.backup
-        mv $HOMEDIR/.config/eww/eww.scss $HOMEDIR/.config/eww/eww.scss.backup
-        ln -s $HOMEDIR/hyprdots/eww/eww.yuck $HOMEDIR/.config/eww/
-        ln -s $HOMEDIR/hyprdots/eww/eww.scss $HOMEDIR/.config/eww/
-        cd eww/
-        for d in $(ls -d */); do
-            mkdir -p $HOMEDIR/.config/eww/$d
-            d="${d::-1}"
-            ln -s $HOMEDIR/hyprdots/eww/$d/$d.yuck $HOMEDIR/.config/eww/$d/
-            ln -s $HOMEDIR/hyprdots/eww/$d/$d.scss $HOMEDIR/.config/eww/$d/
-        done
-        cd -
-    fi
-    changeWallpaper $HOMEDIR/hyprdots/assets/lockscreen.jpg
-
     hyprctl reload
 fi
+
+# For brightness
+read -p "Do you want software for brightness control?[Y/n]" light
+if [[ $light =~ [Yy]$ ]]; then
+    yay -S light
+fi
+
+# For audio control
+read -p "Do you want software for audio control?[Y/n]" audio
+if [[ $audio =~ [Yy]$ ]]; then
+    echo "Installing audio control software..."
+    sudo pacman -S --needed pamixer pipewire-alsa wireplumber pipewire-pulse
+    systemctl --user --now enable pipewire pipewire-pulse wireplumber
+fi
+
+# For screenshotting
+read -p "Do you want software for screenshotting?[Y/n]" screenshots
+if [[ $screenshots =~ [Yy]$ ]]; then
+    echo "Installing screenshotting software..."
+    sudo pacman -S --needed hyprpicker
+    yay -S hyprshot
+fi
+
+# For notification colors
+read -p "Do you want the dunst color scheme to change?[Y/n]" dunst
+if [[ $dunst =~ [Yy]$ ]]; then
+    echo "Configuring notifications..."
+    sudo pacman -S --needed dunst
+    mkdir -p $HOMEDIR/.config/dunst/
+    mv $HOMEDIR/.config/dunst/dunstrc $HOMEDIR/.config/dunst/dunstrc.backup
+    ln -s $HOMEDIR/hyprdots/dunstrc $HOMEDIR/.config/dunst/
+fi
+
+#For the powermenu and certain other widgets
+read -p "Do you want widgets installed?[Y/n]" eww
+if [[ $eww =~ [Yy]$ ]]; then
+    # Widgets
+    read -p "Eww will take a longer time to get installed since it is a rust project. Press any key to proceed." dummie
+    yay -S eww
+    mkdir -p $HOMEDIR/.config/eww
+    mv $HOMEDIR/.config/eww/eww.yuck $HOMEDIR/.config/eww/eww.yuck.backup
+    mv $HOMEDIR/.config/eww/eww.scss $HOMEDIR/.config/eww/eww.scss.backup
+    ln -s $HOMEDIR/hyprdots/eww/eww.yuck $HOMEDIR/.config/eww/
+    ln -s $HOMEDIR/hyprdots/eww/eww.scss $HOMEDIR/.config/eww/
+    cd eww/
+    for d in $(ls -d */); do
+        mkdir -p $HOMEDIR/.config/eww/$d
+        d="${d::-1}"
+        ln -s $HOMEDIR/hyprdots/eww/$d/$d.yuck $HOMEDIR/.config/eww/$d/
+        ln -s $HOMEDIR/hyprdots/eww/$d/$d.scss $HOMEDIR/.config/eww/$d/
+    done
+    cd -
+fi
+changeWallpaper $HOMEDIR/hyprdots/assets/lockscreen.jpg
 
 echo
 
